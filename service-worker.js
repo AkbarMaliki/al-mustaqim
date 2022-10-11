76c59051b2f195ae0597155feb9ee60b258c01d7
Taufik Akbar Maliki <taufikakbarmalikitkj@gmail.com>
2022-10-11 12:06:06 +0800

    update baru

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.633fb38670b434e9fa532a047e302911.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.a3cbaf9ef2b122531971672d073e62ba.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 if (workbox) {
   console.log(`Workbox is loaded`);
